7cfeb603d8f5ca706b600c53682d8d6e50f1cc0c
Linf <2780257110@qq.com>
2019-12-26 10:04:36 +0800

    2019.12.26

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,84 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  //- .home(:style="{'width':windowWidth+'px','min-height':windowHeight+'px'}")
+  .home(:class="active")
+    .left-block(@click="choice('active-left')")
+      .block left
+    .right-block(@click="choice('active-right')")
+      .block right
 </template>
+<style lang="scss" scoped>
+$block-width: 300px;
+.home {
+  overflow-y: scroll;
+  // non-active
+  .left-block,
+  .right-block {
+    width: 1000px;
+    height: 1500px;
+    overflow: hidden;
+    background-color: wheat;
+    transform: scale(0.4, 0.4);
+    position: absolute;
+    top: -350px;
+    transition: all 0.5s;
+  }
+  .left-block {
+    left: auto;
+    right: calc(50% - 200px);
+  }
+  .right-block {
+    left: calc(50% - 200px);
+  }
+  // active
+}
+.home.active-left {
+  .right-block {
+    visibility: hidden;
+  }
+  .left-block {
+    right: calc(50% - 500px);
+    top: 0px;
+    z-index: 10;
+    transform: scale(1, 1);
+  }
+}
+.home.active-right {
+  .left-block {
+    visibility: hidden;
+  }
+  .right-block {
+    left: calc(50% - 500px);
+    top: 0px;
+    z-index: 10;
+    transform: scale(1, 1);
+  }
+}
+</style>
+<script lang="ts">
+// import { Component, Vue } from "vue-property-decorator";
+// export default class App extends Vue {
+//   private windowWidth = window.innerWidth;
+//   private windowHeight = window.innerHeight;
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//   mounted() {
+//     console.log(this.windowWidth,this.windowHeight);
+//   }
+// }
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      active: "",
+      windowWidth: window.innerWidth,
+      windowHeight: window.innerHeight
+    };
+  },
+  mounted() {},
+  methods: {
+    choice(cl) {
+      this.active = this.active === cl ? "" : cl;
+    }
   }
 };
 </script>
