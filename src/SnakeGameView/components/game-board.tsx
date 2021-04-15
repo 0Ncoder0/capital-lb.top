c9233c83ffa5a4086982db01364e632702931bd4
linf <lblinf.dk@foxmail.com>
2021-04-16 00:34:11 +0800

    fix: 修复打包后React报错问题

--- src/SnakeGameView/components/game-board.tsx
+++ src/SnakeGameView/components/game-board.tsx
@@ -2,63 +2,63 @@ import React, { CSSProperties } from "react";
 import GameMap from "../core/game-map";
 import { ItemEnum } from "../core/types";
 
+/** 地图元素 => 颜色 */
+const item2color = new Map([
+  [ItemEnum.Food, "#ff85c0"],
+  [ItemEnum.SnakeBody, "#434343"],
+  [ItemEnum.SnakeHead, "#434343"],
+  [ItemEnum.Space, "#bfbfbf"]
+]);
+
+/** 块样式 */
+const blockStyle = (item: ItemEnum): CSSProperties => ({
+  height: "20px",
+  width: "20px",
+  lineHeight: "20px",
+  textAlign: "center",
+  backgroundColor: item2color.get(item)
+});
+
+/** 行样式 */
+const rowStyle = (): CSSProperties => ({
+  display: "flex"
+});
+
+/** 容器样式 */
+const containerStyle = (): CSSProperties => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  boxSizing: "border-box"
+});
+
+const Blocks = (props: { items: ItemEnum[] }) => (
+  <>
+    {props.items.map((item, index) => (
+      <div key={"block" + index} style={blockStyle(item)} />
+    ))}
+  </>
+);
+
+const Rows = (props: { gameMap: ItemEnum[][] }) => (
+  <>
+    {props.gameMap.map((items, index) => (
+      <div style={rowStyle()} key={"row" + index}>
+        <Blocks items={items} />
+      </div>
+    ))}
+  </>
+);
+
 /** 游戏运行面板 */
 export default class GameBoard extends React.Component {
   props!: { gameMap: GameMap };
 
-  /** 地图元素 => 颜色 */
-  private item2color = new Map([
-    [ItemEnum.Food, "#ff85c0"],
-    [ItemEnum.SnakeBody, "#434343"],
-    [ItemEnum.SnakeHead, "#434343"],
-    [ItemEnum.Space, "#bfbfbf"]
-  ]);
-
-  /** 块样式 */
-  private blockStyle = (item: ItemEnum): CSSProperties => ({
-    height: "20px",
-    width: "20px",
-    lineHeight: "20px",
-    textAlign: "center",
-    backgroundColor: this.item2color.get(item)
-  });
-
-  /** 行样式 */
-  private rowStyle = (): CSSProperties => ({
-    display: "flex"
-  });
-
-  /** 容器样式 */
-  private containerStyle = (): CSSProperties => ({
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    boxSizing: "border-box"
-  });
-
-  private Blocks = (props: { items: ItemEnum[] }) => (
-    <>
-      {props.items.map((item, index) => (
-        <div key={"block" + index} style={this.blockStyle(item)} />
-      ))}
-    </>
-  );
-
-  private Rows = (props: { gameMap: ItemEnum[][] }) => (
-    <>
-      {props.gameMap.map((items, index) => (
-        <div style={this.rowStyle()} key={"row" + index}>
-          <this.Blocks items={items} />
-        </div>
-      ))}
-    </>
-  );
-
   render() {
     return (
-      <div style={this.containerStyle()}>
-        <this.Rows gameMap={this.props.gameMap} />
+      <div style={containerStyle()}>
+        <Rows gameMap={this.props.gameMap} />
       </div>
     );
   }
--- src/SnakeGameView/components/status-controller.tsx
+++ src/SnakeGameView/components/status-controller.tsx
@@ -4,38 +4,38 @@ import { Button } from "antd";
 import SnakeGame from "../core/main";
 import { GameStatusEnum } from "../core/types";
 
+const PauseBtn = (props: { snakeGame: SnakeGame }) => (
+  <Button danger={true} type="primary" onClick={() => props.snakeGame.pause()}>
+    暂停
+  </Button>
+);
+
+const StartBtn = (props: { snakeGame: SnakeGame }) => (
+  <Button type="primary" onClick={() => props.snakeGame.start()}>
+    开始
+  </Button>
+);
+
+const RestartBtn = (props: { snakeGame: SnakeGame }) => (
+  <Button type="primary" onClick={() => props.snakeGame.init().start()}>
+    重新开始
+  </Button>
+);
+
+/** 状态 => 按钮 */
+const status2button = new Map([
+  [GameStatusEnum.Running, PauseBtn],
+  [GameStatusEnum.Pause, StartBtn],
+  [GameStatusEnum.Over, RestartBtn]
+]);
+
 /** 游戏进行状态控制按钮 */
 export default class StatusController extends React.Component {
   public props!: { snakeGame: SnakeGame };
 
-  private PauseBtn = () => (
-    <Button danger={true} type="primary" onClick={() => this.props.snakeGame.pause()}>
-      暂停
-    </Button>
-  );
-
-  private StartBtn = () => (
-    <Button type="primary" onClick={() => this.props.snakeGame.start()}>
-      开始
-    </Button>
-  );
-
-  private RestartBtn = () => (
-    <Button type="primary" onClick={() => this.props.snakeGame.init().start()}>
-      重新开始
-    </Button>
-  );
-
-  /** 状态 => 按钮 */
-  private status2button = new Map([
-    [GameStatusEnum.Running, () => <this.PauseBtn />],
-    [GameStatusEnum.Pause, () => <this.StartBtn />],
-    [GameStatusEnum.Over, () => <this.RestartBtn />]
-  ]);
-
-  private Btn = () => this.status2button.get(this.props.snakeGame.status)?.call(null) || <></>;
-
   render() {
-    return <this.Btn />;
+    const snakeGame = this.props.snakeGame;
+    const Btn = status2button.get(snakeGame.status);
+    return Btn ? <Btn snakeGame={snakeGame} /> : <></>;
   }
 }
