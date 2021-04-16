cc0db00a3f82e10c07ddd923649eb065ee21c7be
0Ncoder0 <35026496+0Ncoder0@users.noreply.github.com>
2021-04-16 23:00:19 +0800

    Update source-code-link.tsx

--- src/SnakeGameView/components/source-code-link.tsx
+++ src/SnakeGameView/components/source-code-link.tsx
@@ -3,7 +3,7 @@ import { GithubOutlined } from "@ant-design/icons";
 /** 源码链接 */
 export class SourceCodeLink extends React.Component {
   render() {
-    const link = "https://github.com/0Ncoder0/capital-lb.top/tree/game-snake-ui/src/SnakeGameView";
+    const link = "https://github.com/0Ncoder0/capital-lb.top/tree/develop/src/SnakeGameView";
     return (
       <div style={{ display: "flex", alignItems: "center" }}>
         <GithubOutlined style={{ color: "black" }} />
