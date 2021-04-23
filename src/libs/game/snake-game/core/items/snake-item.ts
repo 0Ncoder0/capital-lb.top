d04c721572556f1daff36f93bf2ad7334b3da2a1
lblinf.dk@foxmail.com <2780257110@qq.com>
2021-04-23 13:33:03 +0800

    feat: 修改 SnakeItem 模块的方法到 static

--- src/libs/game/snake-game/core/items/snake-item.ts
+++ src/libs/game/snake-game/core/items/snake-item.ts
@@ -1,6 +1,38 @@
 import { Item, State, ItemType, Direction, Position, MapSize } from ".";
 
 export class SnakeItem extends Item {
+  public static readonly Lengthen = (positions: Position[]) => {
+    const length = positions.length;
+    return [...positions, { ...positions[length - 1] }];
+  };
+
+  public static readonly Move = (positions: Position[], direction: Direction, mapSize: MapSize) => {
+    const { Left, Right, Up, Down } = Direction;
+    const { height, width } = mapSize;
+
+    const increasement = new Map([
+      [Left, { x: -1, y: 0 }],
+      [Right, { x: 1, y: 0 }],
+      [Up, { x: 0, y: -1 }],
+      [Down, { x: 0, y: 1 }]
+    ]).get(direction);
+
+    const head = positions[0];
+
+    const newHead = {
+      x: head.x + increasement!.x,
+      y: head.y + increasement!.y
+    };
+
+    if (newHead.x >= width) newHead.x = 0;
+    if (newHead.x < 0) newHead.x = width - 1;
+
+    if (newHead.y >= height) newHead.y = 0;
+    if (newHead.y < 0) newHead.y = height - 1;
+
+    return [newHead, ...positions.slice(0, -1)];
+  };
+
   public mapSize: MapSize;
 
   public direction: Direction;
@@ -10,38 +42,12 @@ export class SnakeItem extends Item {
 
   constructor(data: { direction: Direction; positions: Position[]; mapSize: MapSize }) {
     super({ type: ItemType.Snake });
-    this.direction = data.direction;
-    this.positions = data.positions;
     this.mapSize = data.mapSize;
+    this.direction = data.direction;
+    this.positions = [...data.positions];
   }
 
-  public lengthen = () => {
-    const length = this.positions.length;
-    const tail = { ...this.positions[length - 1] };
-    this.positions = [...this.positions, tail];
-  };
+  public lengthen = () => (this.positions = SnakeItem.Lengthen(this.positions));
 
-  public move = () => {
-    const { Left, Right, Top, Bottom } = Direction;
-    const { height, width } = this.mapSize;
-
-    const increasement = new Map([
-      [Left, { x: -1, y: 0 }],
-      [Right, { x: +1, y: 0 }],
-      [Top, { x: 0, y: -1 }],
-      [Bottom, { x: 0, y: +1 }]
-    ]).get(this.direction);
-
-    const head = this.positions[0];
-    const newHead = {
-      x: head.x + increasement!.x,
-      y: head.y + increasement!.y
-    };
-
-    if (newHead.x >= width) newHead.x = 0;
-    if (newHead.y >= height) newHead.y = 0;
-
-    this.positions.pop();
-    this.positions = [newHead, ...this.positions];
-  };
+  public move = () => (this.positions = SnakeItem.Move(this.positions, this.direction, this.mapSize));
 }
